0b51c76194e8f483bc06ae9710252a3cf9469da8
Yiqi71 <yiqi_c@icloud.com>
2024-10-06 20:05:16 -0400

    edit

--- NoMoreExpired/script.js
+++ NoMoreExpired/script.js
@@ -8,11 +8,16 @@
     const octTop = octSection.getBoundingClientRect();
     const novTop = novSection.getBoundingClientRect();
     const decTop = decSection.getBoundingClientRect();
-    if (novTop.top <= 0 && decTop.top>0) {
+    if (novTop.top <= 20 && decTop.top>20) {
       monthHeader.innerText = 'Nov';
-    } else if(octTop.top<=0 && novTop.top>0) {
+    } else if(octTop.top<=20 && novTop.top>20) {
       monthHeader.innerText = 'Oct';
     } else if(novTop.top<=20){
       monthHeader.innerText = 'Dec';
     }
   });
+
+  document.getElementsByClassName("header").addEventListener("click", function() {
+    const calendar = document.querySelector(".month-calendar");
+    calendar.classList.toggle("hidden");
+});

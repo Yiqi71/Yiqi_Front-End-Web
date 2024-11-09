a01c32f193f3c65a7bf16269e298f603c8a74ec1
Yiqi71 <yiqi_c@icloud.com>
2024-11-09 15:55:34 -0500

    2

--- avatar_info_abuse/createAvatar.js
+++ avatar_info_abuse/createAvatar.js
@@ -1,4 +1,4 @@
-let avatars = ["https://img.freepik.com/premium-vector/cartoon-avatar-2d-vector-design_911078-2173.jpg", "https://img.freepik.com/premium-vector/cartoon-avatar-2d-vector-design_911078-2165.jpg","https://img.freepik.com/premium-vector/cartoon-avatar-2d-vector-design_911078-2169.jpg"];
+let avatars = ["https://img.freepik.com/premium-vector/cartoon-avatar-2d-vector-design_911078-2173.jpg", "https://img.freepik.com/premium-vector/cartoon-avatar-2d-vector-design_911078-2165.jpg", "https://img.freepik.com/premium-vector/cartoon-avatar-2d-vector-design_911078-2169.jpg"];
 displayAvatarDiv.innerHTML = `<img src = "${avatars[0]}" alt = "avatar img" width="300" height = "400">`;
 let nowAvatarN = 0;
 
@@ -7,22 +7,31 @@ changeButton.onclick = function () {
     changeAvatar();
 }
 
-document.getElementById("confirmAvatar").onclick = function(){
+document.getElementById("confirmAvatar").onclick = function () {
     toMainPage();
 }
-function changeAvatar(){
+
+function changeAvatar() {
     let n = Math.floor(Math.random() * avatars.length);
     while (n == nowAvatarN) {
         n = Math.floor(Math.random() * avatars.length);
     }
     displayAvatarDiv.innerHTML = `<img src = "${avatars[n]}" alt = "avatar img" width="300" height = "400">`;
     nowAvatarN = n;
-    console.log("change",nowAvatarN)
+    console.log("change", nowAvatarN)
 }
 
-function toMainPage(){
-    chrome.storage.sync.set({ selectedAvatar: avatars[nowAvatarN] }, function () {
+function toMainPage() {
+    chrome.storage.sync.set({
+        selectedAvatar: avatars[nowAvatarN]
+    }, function () {
         console.log("Avatar saved:", avatars[nowAvatarN]);
-        window.location.href = "mainPage.html";  // Redirect to main page
+        // 设置扩展图标为选定的头像
+        chrome.action.setIcon({
+            path: avatars[nowAvatarN]
+        }, function () {
+            console.log("Extension icon updated.");
+        });
+        window.location.href = "mainPage.html"; // Redirect to main page
     });
 }
